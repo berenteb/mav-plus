f0a8d5f03c07d2f4d83784e704c8105bf661dadf
Bálint Berente <balint.berente@gmail.com>
2022-10-30 20:54:45 +0100

    Removed font print

--- MavPlus/MavPlusApp.swift
+++ MavPlus/MavPlusApp.swift
@@ -2,15 +2,6 @@ import SwiftUI
 
 @main
 struct MavPlusApp: App {
-    init(){
-        for family in UIFont.familyNames {
-             print(family)
-
-             for names in UIFont.fontNames(forFamilyName: family){
-             print("== \(names)")
-             }
-        }
-    }
     var body: some Scene {
         WindowGroup {
             LoadingScreen()
