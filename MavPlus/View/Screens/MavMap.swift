f61f94262ba6fc9f8489d96120aa0d7697404a11
Márton Géza Pfemeter <pfemeter.marton@icloud.com>
2022-11-09 20:13:44 +0100

    adding interaction to map annotations

--- MavPlus/View/Screens/MavMap.swift
+++ MavPlus/View/Screens/MavMap.swift
@@ -6,31 +6,44 @@ struct MavMap: View {
     @StateObject private var model: MapViewModel = MapViewModel()
     
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            Map(coordinateRegion: self.$model.region, annotationItems: self.model.locations) { place in
-                MapAnnotation(coordinate: place.location) {
-                    MapIcon( (place.isStation ? "Station" : "Train") )
+        NavigationStack {
+            ZStack(alignment: .topLeading) {
+                Map(coordinateRegion: self.$model.region, annotationItems: self.model.locations) { place in
+                    MapAnnotation(coordinate: place.location) {
+                        NavigationLink(destination: {
+                            Group {
+                                if (place.isStation) {
+                                    StationDetailsScreen(code: place.id)
+                                } else {
+                                    TrainDetailsScreen(trainId: Int(place.id) ?? 0)
+                                }
+                            }
+                        }, label: {
+                            
+                            MapIcon( (place.isStation ? "Station" : "Train") )
+                        })
+                    }
                 }
+                .edgesIgnoringSafeArea(.top).overlay(alignment: .top){
+                    Rectangle().frame(height:0).background(.regularMaterial)
+                }
+                .onAppear{
+                    model.startTimer()
+                }
+                .onDisappear{
+                    model.stopTimer()
+                }
+                
+                VStack(alignment: .trailing) {
+                    Text("Show stations")
+                        .bold()
+                        .padding(5)
+                        .background(Color("Secondary"))
+                        .cornerRadius(10)
+                    Toggle("", isOn: self.$model.showStations)
+                }
+                .padding()
             }
-            .edgesIgnoringSafeArea(.top).overlay(alignment: .top){
-                Rectangle().frame(height:0).background(.regularMaterial)
-            }
-            .onAppear{
-                model.startTimer()
-            }
-            .onDisappear{
-                model.stopTimer()
-            }
-            
-            VStack(alignment: .trailing) {
-                Text("Show stations")
-                .bold()
-                .padding(5)
-                .background(Color("Secondary"))
-                .cornerRadius(10)
-                Toggle("", isOn: self.$model.showStations)
-            }
-            .padding()
         }
     }
 }
