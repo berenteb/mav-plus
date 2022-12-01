71a2a51a20a34ab3cf459e7b3e385e90d1f42087
Márton Géza Pfemeter <pfemeter.marton@icloud.com>
2022-12-01 16:11:21 +0100

    adding commenting for first most view components

--- MavPlus/View/Components/Common/CustomMapAnnotation.swift
+++ MavPlus/View/Components/Common/CustomMapAnnotation.swift
@@ -11,20 +11,32 @@ import SwiftUI
 
 let clusterID = "clustering"
 
+/// Custom annotation view using MapIcon SwiftUI component
 class CustomMapAnnotation: MKAnnotationView {
-
-    static let ReuseID = "cultureAnnotation"
-
+    
+    /// ID for MapKit annotation reusability
+    static let ReuseID = "customAnnotation"
+    
+    /// Default initializer
+    /// - Parameters:
+    ///   - annotation: The data behind this view
+    ///   - reuseIdentifier: Optional, is overriden by self.ReuseID
+    ///   - isStation: Whether to use the station or the train icon in the view
     init(annotation: MKAnnotation?, reuseIdentifier: String?, isStation: Bool) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         clusteringIdentifier = clusterID
         self.addSubview(UIHostingController(rootView: MapIcon(self.getImageName(isStation: isStation))).view)
     }
-
+    
+    /// Initializer for protocol conformance - not implemented
+    /// - Parameter aDecoder: The decoder to initialize from
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /// Returns the name of the image asset for initalization
+    /// - Parameter isStation: True if the station image's name is required, false if the name of the train image
+    /// - Returns: If isStation then station image's name, otherwise the name of the train image
     private func getImageName(isStation: Bool) -> String {
         if (isStation) {
             return "Station"
@@ -32,7 +44,8 @@ class CustomMapAnnotation: MKAnnotationView {
             return "Train"
         }
     }
-
+    
+    /// Sets the displayPriority attribute of this object to .defaultLow
     override func prepareForDisplay() {
         super.prepareForDisplay()
         displayPriority = .defaultLow
--- MavPlus/View/Components/Common/IconField.swift
+++ MavPlus/View/Components/Common/IconField.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
+/// A UI component with text and an image
 struct IconField: View {
+    
+    /// Name of the displayed image
     @State var iconName: String
+    
+    /// Text to display
     @State var value: String
     
+    /// SwiftUI view generation.
     var body: some View {
         HStack{
             Image(systemName: iconName).foregroundColor(Color("Secondary"))
@@ -12,7 +18,10 @@ struct IconField: View {
     }
 }
 
+/// SwiftUI Preview
 struct IconField_Previews: PreviewProvider {
+    
+    /// SwiftUI Preview content generation.
     static var previews: some View {
         IconField(
             iconName: "arrow.left.arrow.right",
