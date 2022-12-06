4f8d0054879d89e852702267e8635a69dda2b077
Márton Géza Pfemeter <pfemeter.marton@icloud.com>
2022-12-06 10:30:56 +0100

    documenting RssRepository

--- MavPlus/Repository/RssRepository.swift
+++ MavPlus/Repository/RssRepository.swift
@@ -2,35 +2,69 @@ import Foundation
 import WebKit
 import Combine
 
+/// Protocol for accessing RssRepository
 protocol RssProtocol: RequestStatus, Updateable, XMLParserDelegate {
+    
+    /// Getting singleton instance
     static var shared: RssProtocol {get}
+    
+    /// The parsed list of RssItems
     var rssItemList: [RssItem] {get}
+    
+    // TODO
     var publisher: PassthroughSubject<RssFields, Never> { get }
 }
 
+/// Enum for the tags to parse
 enum InterestingTag: String {
+    /// Title of an item
     case title
+    
+    /// Link to the item
     case link
 }
 
+/// Data type for the result of RSS parsing
 struct RssFields {
+    
+    /// The parsed list of RssItems
     var rssItemList: [RssItem]
+    
+    /// Whether the parsing resulted in an error
     var isError: Bool
+    
+    /// Whether the parsing is still loading
     var isLoading: Bool
 }
 
+/// Class parsing RSS feed
 public class RssRepository: NSObject, RssProtocol {
+    
+    /// Singleton instance
     static var shared = RssRepository() as (any RssProtocol)
+    
+    /// The parsed list of RssItems
     var rssItemList: [RssItem]
+    
+    /// Whether the parsing resulted in an error
     var isError: Bool
+    
+    /// Whether the parsing is still loading
     var isLoading: Bool
     
+    /// Last tag visited during parsing
     private var lastTag: String?
+    
+    /// String accumulating the content of the tags since the last valid tag
     private var lastContent: String?
+    
+    /// The items parsed so far
     private var parseList: [RssItem]
     
+    // TODO
     var publisher = PassthroughSubject<RssFields, Never>()
     
+    /// Default initializer, calls self.update()
     public override init() {
         self.rssItemList = []
         self.isError = false
@@ -42,6 +76,7 @@ public class RssRepository: NSObject, RssProtocol {
         update()
     }
     
+    /// Updates self.rssItemList by downloading and parsing the RSS feed again
     public func update() {
         self.isLoading = true
         self.parseList.removeAll()
@@ -67,6 +102,9 @@ public class RssRepository: NSObject, RssProtocol {
         task.resume()
     }
     
+    /// Function starting the parsing of the RSS feed
+    /// - Parameter inputData: The data received at downloading the RSS feed
+    /// - Returns: Nothing
     private func startParsing(inputData: Data?) -> Void {
         if let realInputData: Data = inputData {
             let parser: XMLParser = XMLParser(data: realInputData)
@@ -87,6 +125,8 @@ public class RssRepository: NSObject, RssProtocol {
         }
     }
     
+    /// Handles a tag in the RSS feed
+    /// - Returns: Nothing
     private func handleNewTag() -> Void {
         if let actualTag = self.lastTag, let actualContent = self.lastContent {
             switch actualTag {
@@ -109,6 +149,11 @@ public class RssRepository: NSObject, RssProtocol {
         }
     }
     
+    /// Called during the parsing of the RSS feed, updates self.lastContent
+    /// - Parameters:
+    ///   - parser: The parser parsing the RSS feed
+    ///   - string: The content encountered during parsing
+    /// - Returns: Nothing
     public func parser(
         _ parser: XMLParser,
         foundCharacters string: String
@@ -122,6 +167,14 @@ public class RssRepository: NSObject, RssProtocol {
         }
     }
     
+    /// Called during the parsing of the RSS feed, at the opening of a new tag
+    /// - Parameters:
+    ///   - parser: Teh parser parsing the RSS feed
+    ///   - elementName: The name of the tag started
+    ///   - namespaceURI: URI of the namespace
+    ///   - qName: Qualified name
+    ///   - attributeDict: Dictionary of the attributes of the tag
+    /// - Returns: Nothing
     public func parser(
         _ parser: XMLParser,
         didStartElement elementName: String,
