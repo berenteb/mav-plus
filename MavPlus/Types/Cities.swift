5d3e9f12d39e04b2a42dfd881de5595778bec122
Márton Géza Pfemeter <pfemeter.marton@icloud.com>
2022-12-06 08:54:05 +0100

    documenting types part 1

--- MavPlus/Types/Cities.swift
+++ MavPlus/Types/Cities.swift
@@ -1,5 +1,10 @@
+/// Data type representing a city
 struct City: Codable {
+    
+    /// Name of the city
     let cityName: String
+    
+    /// List of postal codes in the city
     let postalCodes: [String]
 }
 
